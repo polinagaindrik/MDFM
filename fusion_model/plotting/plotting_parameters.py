a7e301ac78c046f46ac040894bf097dcf44f8071
Polina Gaindrik <polinagayndrik@gmail.com>
2025-08-19 18:15:52 +0200

    plotting: plot parameters for all bacteria (not excluding last one/rest)

--- fusion_model/plotting/plotting_parameters.py
+++ fusion_model/plotting/plotting_parameters.py
@@ -13,7 +13,7 @@ def plot_alpha(alph0, alph1, bact_all, clrs1, path=''):
     temp = np.linspace(1.95, 14.05, 100)
 
     fig, ax = plt.subplots()
-    for a0, a1, b in zip(alph0[:-1], alph1[:-1], bact_all[:-1]):
+    for a0, a1, b in zip(alph0, alph1, bact_all):
         #ax.plot(temp, lin_func(temp, a0, a1)/24., label=b, color=clrs1[b], linewidth=2)
         ax.plot(temp, exp_func(temp, a0, a1)/24., label=b, color=clrs1[b], linewidth=2)
     ax.set_title('Wachstumsrate', fontsize=14)
@@ -34,10 +34,10 @@ def plot_lambda(lambd, bact_all, clrs1, path=''):
     fig, ax = plt.subplots()
     #ax.stem(bact_all[:-1], 10**lambd[:-1]/24.)
     (lambd_1, lambd_exp) = lambd
-    ax.stem(bact_all[:-1], 10**lambd_1[:-1]/24.)
-    ax.set_xticks(np.linspace(0, n_cl-2, n_cl-1))
+    ax.stem(bact_all, 10**lambd_1/24.)
+    ax.set_xticks(np.linspace(0, n_cl-1, n_cl))
     ax.set_title('Lag-Phasenübergangsrate', fontsize=14)
-    ax.set_xticklabels(bact_all[:-1], rotation=45, ha='right')
+    ax.set_xticklabels(bact_all, rotation=45, ha='right')
     ax.tick_params(axis='both', which='major', labelsize=14)
     ax.set_ylabel(r'$\lambda$ [$h^{-1}$]', fontsize=14)
     ax.tick_params(axis='both', which='major', labelsize=12)
@@ -46,10 +46,10 @@ def plot_lambda(lambd, bact_all, clrs1, path=''):
     fig, ax = plt.subplots()
     #ax.stem(bact_all[:-1], 10**lambd[:-1]/24.)
     (lambd_1, lambd_exp) = lambd
-    ax.stem(bact_all[:-1], lambd_exp[:-1])
-    ax.set_xticks(np.linspace(0, n_cl-2, n_cl-1))
+    ax.stem(bact_all, lambd_exp)
+    ax.set_xticks(np.linspace(0, n_cl-1, n_cl))
     ax.set_title('Lag-Phasenübergangsrate', fontsize=14)
-    ax.set_xticklabels(bact_all[:-1], rotation=45, ha='right')
+    ax.set_xticklabels(bact_all, rotation=45, ha='right')
     ax.tick_params(axis='both', which='major', labelsize=14)
     ax.set_ylabel(r'$\lambda$ [$h^{-1}$]', fontsize=14)
     ax.tick_params(axis='both', which='major', labelsize=12)
@@ -60,7 +60,7 @@ def plot_lambda(lambd, bact_all, clrs1, path=''):
     temp = np.linspace(1.95, 14.05, 100)
 
     fig, ax = plt.subplots()
-    for a0, a1, b in zip(lambd_1[:-1], lambd_exp[:-1], bact_all[:-1]):
+    for a0, a1, b in zip(lambd_1, lambd_exp, bact_all):
         #ax.plot(temp, lin_func(temp, a0, a1)/24., label=b, color=clrs1[b], linewidth=2)
         ax.plot(temp, exp_func2(temp, a0, a1)/24., label=b, color=clrs1[b], linewidth=2)
     ax.set_title('Lag-Phasenübergangsrate', fontsize=14)
@@ -77,13 +77,13 @@ def plot_lambda(lambd, bact_all, clrs1, path=''):
 def plot_kij(kij_matrix, bact_all, path=''):
     n_cl = len(bact_all)
     fig, ax = plt.subplots()
-    im = ax.imshow(kij_matrix[:-1, :-1])
+    im = ax.imshow(kij_matrix)
     fig.colorbar(im, orientation='vertical')
     ax.set_title(r'Inhibitionskoeffizienten $k_{ij}$', fontsize=14)
-    ax.set_xticks(np.linspace(0, n_cl-2, n_cl-1))
-    ax.set_xticklabels(bact_all[:-1], rotation=45, ha='right')
-    ax.set_yticks(np.linspace(0, n_cl-2, n_cl-1))
-    ax.set_yticklabels(bact_all[:-1], rotation=45, ha='right')
+    ax.set_xticks(np.linspace(0, n_cl-1, n_cl))
+    ax.set_xticklabels(bact_all, rotation=45, ha='right')
+    ax.set_yticks(np.linspace(0, n_cl-1, n_cl))
+    ax.set_yticklabels(bact_all, rotation=45, ha='right')
     ax.tick_params(axis='both', which='major', labelsize=12)
     plt.savefig(path+'kij.png', bbox_inches='tight')
     plt.close(fig)
@@ -110,7 +110,7 @@ def plot_x0(x0_vals, bact_all, exps, path=''):
 def plot_parameters(param_ode, bact_all, exps, clrs1, path=''):
     n_cl = len(bact_all)
     bact_all = [b if b!='Others' else 'Rest' for b in bact_all]
-    bact_all[-1] = 'Rest'
+    #bact_all[-1] = 'Rest'
     param = param_ode[n_cl*len(exps):]
     #lambd_exp = param[:n_cl]#10**param[:n_cl]
     #alph0 = param[n_cl:2*n_cl]

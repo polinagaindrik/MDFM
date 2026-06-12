a52f5dced0f031f14acbcc31f2e907bce9b963d4
polina.gaindrik <polinagayndrik@gmail.com>
2026-06-12 15:43:37 +0200

    parameter distribution: update estimating deistribution from observables

--- examples/main_param_distrib.py
+++ examples/main_param_distrib.py
@@ -7,6 +7,30 @@ import fusion_model as fm
 import pandas as pd
 import numpy as np
 
+def cost_withS_withdiffalpha(param, calibr_setup, jac_spasity):
+    n_cl = calibr_setup['n_cl']
+    n_media = calibr_setup['n_media']
+    exps = calibr_setup['exps']
+    lambd  = param[n_cl:n_cl+n_cl]
+    alph = param[n_cl+n_cl:n_cl+n_cl + n_cl*len(exps)]
+    rest_ode_param = param[n_cl+n_cl + n_cl*len(exps):-n_cl*n_media]
+    s_x = np.array(param)[-n_cl*n_media:].reshape((n_media, n_cl))
+    x0_vals = param[:n_cl]#*len(exps)]
+
+    (df_mibi, df_maldi, df_ngs, ) = calibr_setup['dfs']
+    days, [obs_mibi_meas, obs_maldi_meas, obs_ngs_meas] = calibr_setup['data_array']
+    n_cl = np.shape(obs_maldi_meas)[2] #np.shape(df_maldi)[0]
+    exps = sorted(list(set([s.split('_')[0] for s in df_mibi.columns])))
+    mibi_max = np.nanmax(obs_mibi_meas, axis=(0, 1))**2
+    ll_ngs, ll_maldi, ll_mibi = np.zeros(np.shape(obs_ngs_meas)), np.zeros(np.shape(obs_maldi_meas)), np.zeros(np.shape(obs_mibi_meas))
+    for i, exp in enumerate(exps):
+        param_ode = np.concatenate((lambd, alph[n_cl*i:n_cl*(i+1)], rest_ode_param))
+        ll_ngs[i], ll_maldi[i], ll_mibi[i] = fm.pest.sq_diff_oneexp(calibr_setup, exp, i, n_cl, x0_vals, param_ode, s_x, mibi_max)
+    ll_ngs = ll_ngs[ll_ngs!=0]
+    ll_maldi = ll_maldi[ll_maldi!=0]
+    ll_mibi = ll_mibi[ll_mibi!=0]
+    return calibr_setup['aggregation_func']([ll_ngs, ll_maldi, ll_mibi])
+
 
 def prepare_insilico_data(insilico_model, n_cl, temps, ntr, S_matrix_setup, param_ode=None, x10=None, path='', inhib=False, noise=0., rel_noise=.0, cutoff=0., cutoff_prop=0., add_name='', exp_start_offset=0):
     data = insilico_model(n_cl, temps, ntr, S_matrix_setup, param_ode=param_ode, path=path, inhib=inhib, noise=noise, rel_noise=rel_noise, x10=x10, add_name=add_name, exp_start_offset=exp_start_offset)
@@ -90,7 +114,7 @@ def data_calibration_distribution(dfs, path=''):
         }
     fm.output.json_dump(calibr_presetup['exp_temps'], 'exp_temps_model_paper.json', dir=path)
 
-    x0_bnds_all = tuple([(1., 4.5) for _ in range (calibr_presetup['n_cl']) for _ in range (len(calibr_presetup['exps']))])
+    x0_bnds_all = tuple([(1., 4.5) for _ in range (calibr_presetup['n_cl'])])
     inhib_bnds = [(0.02, 3.) for _ in range (n_cl*(n_cl-1))]
     param_ode_bnds = tuple([(1e-6, 1e-2) for _ in range (n_cl)] + # lambd
                             [(.01,  4.) for _ in range (n_cl) for _ in range (len(calibr_presetup['exps']))]  + # alph
@@ -101,7 +125,7 @@ def data_calibration_distribution(dfs, path=''):
     calibr_setup['param_bnds'] =  x0_bnds_all + param_ode_bnds + S_bnds
 
     print('Start optimization...')
-    param_opt = fm.pest.calculate_model_params(fm.pest.cost_withS_withdiffalpha, calibr_setup)[0]
+    param_opt = fm.pest.calculate_model_params(cost_withS_withdiffalpha, calibr_setup)[0]
     s_x = np.array(param_opt)[-n_cl*n_media:].reshape((n_media, n_cl))
     calibr_setup['s_x'] = s_x
     fm.output.json_dump({'param_ode': param_opt[:-n_cl*n_media].astype(list), 's_x': s_x.flatten(), 'T_x': T_x}, 'Result_calibration.json', dir=path_new)
@@ -138,9 +162,6 @@ def estimate_parameter_set(param_init, s_x, n_cl, add_name='',path=''):
     fm.output.json_dump(calibr_presetup['exp_temps'], 'exp_temps_model_paper.json', dir='model_paper/')
 
     x0_bnds_all = tuple([(1., 4.5) for _ in range (calibr_presetup['n_cl']) for _ in range (len(calibr_presetup['exps']))])
-    #inhib_bnds = [(0.02, 3.) if i!=j else (0., 0.)
-    #              for i in range (n_cl)
-    #              for j in range (n_cl)]
     inhib_bnds = [(0.02, 3.) for _ in range (n_cl*(n_cl-1))]
     param_ode_bnds = tuple([(1e-6, 1e-2) for _ in range (n_cl)] + # lambd
                            [(.01,  4.) for _ in range (n_cl)]   +  # alph
@@ -159,11 +180,11 @@ if __name__ == "__main__":
     n_cl = 2
     n_media = 2
     relnoise = 0.1
+    n_exps = 3
 
     add_name = ''
-    path_new = path+'main_param_distrib2/'
+    path_new = path+f'main_param_distrib_{int(n_exps)}exp/'
     # 'Real' model:
-    n_exps = 50
     temps = [2. for _ in range (n_exps)]
     ntr = 1
 
@@ -176,25 +197,7 @@ if __name__ == "__main__":
     param_ode = np.array([  8e-4,   5e-5,   # 3e-3,
                           np.nan, np.nan,   # 1.2,
                            7.,              # identifiable at 1.3e7
-                          2.5e-8, 9.5e-8])
+                          2.5e-1, 9.5e-1])
     
     dfs_calibr = data_generation_distribution(n_exps, n_cl, param_ode, s_x, path=path_new)
     param_opt, calibr_setup = data_calibration_distribution(dfs_calibr, path=path_new)
-
-    '''
-    for i in range (n_exps):
-        add_name = f'_{i}'
-        alph_sampled = np.random.lognormal(mean=0., sigma=0.5, size=n_cl)
-        param_ode[n_cl:n_cl*2] = alph_sampled
-        param_init = param_ode
-        param_opt, calibr_setup = estimate_parameter_set(param_init, s_x, n_cl, add_name=add_name, path=path_new)
-
-        s_x = np.array(param_opt)[-n_cl*n_media:].reshape((n_media, n_cl))
-        calibr_setup['s_x'] = s_x
-
-        fm.output.json_dump({'param_ode': param_opt[:-n_cl*n_media].astype(list), 's_x': s_x.flatten(), 'T_x': T_x}, f'Result_calibration{add_name}.json', dir=path_new)
-        #param_opt = fm.output.read_from_json(''+f'Result_calibration{add_name}.json', dir=path_new)['param_ode']
-
-        # TODO: Consider: Now we estimate the whole parameter set for each run, but only alpha changes. Maybe estimate all exps together but with multiple alpha params.
-        # e.g param_estim = (x0, alph_1_exp1, alph_2_exp1, ..., alph_1_exp2, alph_2_exp2, ... , param_ode_other)
-    '''
--- examples/plot_model_results.py
+++ examples/plot_model_results.py
@@ -12,24 +12,30 @@ if __name__ == "__main__":
     n_cl = 2
     n_media = 2
     relnoise = 0.
+    n_exps = 3
 
-    path = 'out/main_param_distrib/'
+    path = f'out/main_param_distrib2_{int(n_exps)}exp/'
     path2=path
-    exp_temps = fm.output.read_from_json(''+'exp_temps_model_paper.json', dir='out/main_param_distrib/')
+    exp_temps = fm.output.read_from_json(''+'exp_temps_model_paper.json', dir=path2)
 
     # Calibration result:
     res = fm.output.read_from_json('Result_calibration.json', dir=path2)
     T_x = res['T_x']
     param_opt = res['param_ode']
     #param_ode = param_opt[:-n_cl*n_media]
-    x0_vals = param_opt[:n_cl*len(exp_temps)]
-    lambd_opt = param_opt[n_cl*len(exp_temps):n_cl*len(exp_temps)+n_cl]
-    alph_opt = param_opt[n_cl + n_cl*len(exp_temps):n_cl + 2*n_cl*len(exp_temps)]
-    rest_ode_param = param_opt[n_cl + 2*n_cl*len(exp_temps):]
+    x0_vals = param_opt[:n_cl]#x0_vals = param_opt[:n_cl]*len(exp_temps)]
+    lambd_opt = param_opt[n_cl:n_cl+n_cl] #lambd_opt = param_opt[n_cl*len(exp_temps):n_cl*len(exp_temps)+n_cl]
+    print(f"Optimized lambda: {lambd_opt}")
+    alph_opt = param_opt[n_cl + n_cl:2*n_cl + n_cl*len(exp_temps)]
+    #alph_opt = param_opt[n_cl + n_cl*len(exp_temps):n_cl + 2*n_cl*len(exp_temps)]
+    rest_ode_param = param_opt[2*n_cl + n_cl*len(exp_temps):]
+    #rest_ode_param = param_opt[n_cl + 2*n_cl*len(exp_temps):]
+    print(f"Optimized alpha: {alph_opt}")
+    print(f"n_max: {rest_ode_param[0]}")
     s_x = np.array(res['s_x']).reshape((n_media, n_cl))
 
-    n_exps = 9
     alph_exps = []
+    alph_opt_vals = []
     for k in range (n_exps):
         add_name = f'_{k}'
         df_names = [f'dataframe_mibi{add_name}.pkl', f'dataframe_maldi{add_name}.pkl', f'dataframe_ngs{add_name}.pkl']
@@ -42,34 +48,28 @@ if __name__ == "__main__":
         res_exp = fm.output.read_from_json(f'Result_real_paramdistrib_{k}.json', dir=path2)
         param_exp = np.array(res_exp['param_ode'])
         alph_exps.append(param_exp[2*n_cl:2*n_cl+n_cl])
+        lambd_exp = param_exp[n_cl:2*n_cl]
+        rest_ode_param_exp = param_exp[2*n_cl+n_cl:]
+        alph_opt_vals.append(alph_opt[k*n_cl:(k+1)*n_cl])
 
 
-        '''
-        step = 1
-        optim_file2 = f"optimization_history{int(step)}.csv"
-        df_optim2 = pd.read_csv(path+optim_file2)
-        T_x = [0.] +[1. for _ in range(n_cl-1)]
-        # Take optimal parameter values on last optimization step
-        param_opt = df_optim2.T[df_optim2.T.columns[-1]].values[1:-1]
-        s_x = np.array(param_opt)[-n_cl*n_media:].reshape((n_media, n_cl))
-        param_ode = param_opt[:-n_cl*n_media]
-        '''
         # Plot resulting model
         calibr_setup={
                 'model': fm.mdl.fusion_model_distr,
                 'T_x': T_x,
                 'output_path': path2,
-                'exp_temps': fm.output.read_from_json(''+'exp_temps_model_paper.json', dir='out/main_param_distrib/'),
+                'exp_temps': fm.output.read_from_json(''+'exp_temps_model_paper.json', dir=path2),
                 's_x': s_x,
                 'media': media, 
         }
 
-        param_ode = np.concatenate((x0_vals[k*n_cl:(k+1)*n_cl], lambd_opt, alph_opt[k*n_cl:(k+1)*n_cl], rest_ode_param))
-
+        param_ode = np.concatenate((x0_vals, lambd_opt, alph_opt[k*n_cl:(k+1)*n_cl], rest_ode_param))
         t_model = np.linspace(0., 18., 100)
+
         x_count, obs_mibi_model, obs_maldi_model, obs_ngsi_model, temps_model = fm.mdl.calc_obs_model(fm.dtf.filter_dataframe(f'V0{k+1}', data), param_ode, calibr_setup, t_model)
         exps = sorted(list(set([s.split('_')[0] for s in data[0].columns])))
         
+        '''
         exp_clrs = {}
         i, j, k = 0, 0, 0
         for exp, temp in zip(calibr_setup['exp_temps'], calibr_setup['exp_temps'].values()):
@@ -82,6 +82,10 @@ if __name__ == "__main__":
             else:
                 exp_clrs[exp] =  fm.plotting.red_colors[k]
                 k += 1
+        '''
+        exp_clrs = {}
+        for exp in calibr_setup['exp_temps']:
+            exp_clrs[exp] = fm.plotting.blue_colors[0]
 
         labels = ('Time',  r'log CFU mL$^{-1}$')
         '''
@@ -108,10 +112,56 @@ if __name__ == "__main__":
 
     # Calculate mu sigma for alpha distributions
     shape, loc, scale = stats.lognorm.fit(alph_opt, floc=0)
-    mu, sigma = np.log(scale), shape
-    print(f"Optimized: Mu: {mu}, Sigma: {sigma}")
+    mu_opt, sigma_opt = np.log(scale), shape
+    print(f"Optimized: Mu: {mu_opt}, Sigma: {sigma_opt}")
 
     # Calculate mu sigma for alpha distributions
     shape, loc, scale = stats.lognorm.fit(np.array(alph_exps).flatten(), floc=0)
     mu, sigma = np.log(scale), shape
     print(f"Data: Mu: {mu}, Sigma: {sigma}")
+
+
+    fig, ax = plt.subplots()
+    x = np.linspace(0., 5, 100)
+    pdf = stats.lognorm.pdf(x, s=sigma_opt, scale=np.exp(mu_opt))
+    ax.plot(x, pdf, label=f'Estimated distribution\n (mu={mu_opt:.2f}, sigma={sigma_opt:.2f})')
+    mu, sigma = 0.5, 0.5
+    x = np.linspace(0., 5, 100)
+    pdf = stats.lognorm.pdf(x, s=sigma, scale=np.exp(mu))
+    ax.plot(x, pdf, label=f'"Real" distribution\n (mu={mu:.2f}, sigma={sigma:.2f})')
+    plt.xlabel('Alpha')
+    plt.legend()
+    plt.savefig(path2+'alph_distribution.png', bbox_inches='tight')
+    plt.close(fig)
+
+
+    fig, ax = plt.subplots()
+    sct_mrk = ['o', 'x', '^', 's', 'D', 'P', '*', 'h', 'v', '<', '>']
+    tick_lbls = [r'$\lambda_1$', r'$\lambda_2$', r'$n_{\max}$', r'k$_{12}$', r'k$_{21}$']
+    param_plot_opt = np.concatenate((lambd_opt, rest_ode_param))
+    param_plot_exps = np.concatenate((lambd_exp, rest_ode_param_exp))
+    ax.scatter(np.linspace(1, len(param_plot_opt), len(param_plot_opt)), param_plot_opt, marker=sct_mrk[i])
+    ax.scatter(np.linspace(1, len(param_plot_exps), len(param_plot_exps)), param_plot_exps, marker=sct_mrk[i])
+    ax.set_yscale('log')
+    plt.legend(('Optimized', 'Exps'))
+    plt.ylabel('Parameter value')
+    ticks_val = np.linspace(1, len(param_plot_opt), len(param_plot_opt))
+    ax.set_xticks(ticks_val)
+    ax.set_xticklabels(tick_lbls)
+    plt.savefig(path2+'param_opt_vs_exps.png', bbox_inches='tight')
+    plt.close(fig)
+
+    alph_exps = np.array(alph_exps)
+    alph_opt_vals = np.array(alph_opt_vals)
+    fig, ax = plt.subplots()
+    for i in range (n_cl):
+        ax.scatter(np.linspace(1, n_exps, n_exps), alph_opt_vals[:, i], marker=sct_mrk[i], label=f'Optimized {i+1} Sp.')
+        ax.scatter(np.linspace(1, n_exps, n_exps), alph_exps[:, i], marker=sct_mrk[i], label=f'Exps {i+1} Sp.')
+    ax.set_yscale('log')
+    plt.ylabel('Alpha value')
+    plt.xlabel('Experiment Number')
+    ticks_val = np.linspace(1, n_exps, n_exps)
+    ax.set_xticks(ticks_val)
+    plt.legend(framealpha=0.5, bbox_to_anchor=(1, 1.05))
+    plt.savefig(path2+'alpha_comparison_opt_vs_exps.png', bbox_inches='tight')
+    plt.close(fig)

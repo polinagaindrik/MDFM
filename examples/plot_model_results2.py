82b013b6efb38847b4afb658234f2fd21bca3820
polina.gaindrik <polinagayndrik@gmail.com>
2026-06-13 18:10:04 +0200

    Parameter distribution: for results plotting add reading param_opt from optimization df

--- examples/plot_model_results2.py
+++ examples/plot_model_results2.py
@@ -60,17 +60,28 @@ if __name__ == "__main__":
     plt.rcParams['figure.dpi'] = 500
     n_cl = 2
     n_media = 2
-    relnoise = 0.1
+    relnoise = 0.
     n_exps = 5
 
-    path = f'out/main_param_distrib2_{int(n_exps)}exp_divx/'
-    path2=path
+    path2 = f'out/main_param_distrib2_{int(n_exps)}exp_divx/'
     exp_temps = fm.output.read_from_json(''+'exp_temps_model_paper.json', dir=path2)
 
     # Calibration result:
+    # Read from dictionary:
     res = fm.output.read_from_json('Result_calibration.json', dir=path2)
     T_x = res['T_x']
     param_opt = res['param_ode']
+    s_x = np.array(res['s_x']).reshape((n_media, n_cl))
+        
+    ## Or get optimal parameters from df:
+    '''
+    df_optim2 = pd.read_csv(path2+"optimization_history1.csv")
+    param_opt = df_optim2.T[df_optim2.T.columns[-1]].values[1:-1-n_cl*n_media]
+    s_x = df_optim2.T[df_optim2.T.columns[-1]].values[-1-n_cl*n_media:-1]
+    s_x = np.array(s_x).reshape((n_media, n_cl))
+    T_x = np.ones((n_cl))
+    '''
+
     #param_ode = param_opt[:-n_cl*n_media]
     x0_vals = param_opt[:n_cl]#x0_vals = param_opt[:n_cl]*len(exp_temps)]
     lambd_opt = param_opt[n_cl:n_cl+n_cl] #lambd_opt = param_opt[n_cl*len(exp_temps):n_cl*len(exp_temps)+n_cl]
@@ -81,7 +92,6 @@ if __name__ == "__main__":
     #rest_ode_param = param_opt[n_cl + 2*n_cl*len(exp_temps):]
     print(f"Optimized alpha: {alph_opt}")
     print(f"n_max: {rest_ode_param[0]}")
-    s_x = np.array(res['s_x']).reshape((n_media, n_cl))
 
     n_exps = len(exp_temps)
     alph_exps = []
@@ -101,16 +111,6 @@ if __name__ == "__main__":
         rest_ode_param_exp = param_exp[2*n_cl+n_cl:]
         alph_opt_vals.append(alph_opt[k*n_cl:(k+1)*n_cl])
 
-        '''
-        step = 1
-        optim_file2 = f"optimization_history{int(step)}.csv"
-        df_optim2 = pd.read_csv(path+optim_file2)
-        T_x = [0.] +[1. for _ in range(n_cl-1)]
-        # Take optimal parameter values on last optimization step
-        param_opt = df_optim2.T[df_optim2.T.columns[-1]].values[1:-1]
-        s_x = np.array(param_opt)[-n_cl*n_media:].reshape((n_media, n_cl))
-        param_ode = param_opt[:-n_cl*n_media]
-        '''
         # Plot resulting model
         calibr_setup={
                 'model': fm.mdl.fusion_model_distr,
@@ -130,37 +130,12 @@ if __name__ == "__main__":
         x_count_init, obs_mibi_model_init, obs_maldi_model_init, obs_ngsi_model_init, temps_model_init = fm.mdl.calc_obs_model(fm.dtf.filter_dataframe(f'V{k+1:02d}', data[:-1]), param_ode_init, calibr_setup, t_model)
         exps = sorted(list(set([s.split('_')[0] for s in data[0].columns])))
         
-        '''
-        exp_clrs = {}
-        i, j, k = 0, 0, 0
-        for exp, temp in zip(calibr_setup['exp_temps'], calibr_setup['exp_temps'].values()):
-            if temp == 2.:
-                exp_clrs[exp] =  fm.plotting.blue_colors[i]
-                i += 1
-            elif temp == 6.:
-                exp_clrs[exp] =  fm.plotting.green_colors[j]
-                j += 1
-            else:
-                exp_clrs[exp] =  fm.plotting.red_colors[k]
-                k += 1
-        '''
         exp_clrs = {}
         for exp in calibr_setup['exp_temps']:
             exp_clrs[exp] = fm.plotting.blue_colors[0]
-
         labels = ('Time',  r'log CFU mL$^{-1}$')
-        '''
-        for j, med in enumerate(media):
-            fm.plotting.plot_all([2., 6., 10.], labels, templ_meas=fm.plotting.plot_measurements_insilico, df=data[0].filter(like=med), clrs=exp_clrs,
-                    temps=temps_model, mtimes=t_model, mestim=obs_mibi_model[:,j, : ], dir=path2, add_name=f'MiBi_{med}_const_model'+add_name, time_lim=[17.5, 17.5, 17.5])
 
-
-        for j, med in enumerate(media):
-            for i, temp in enumerate([2.,]):
-                obs_mibi = obs_mibi_model[3*i:3*i+3,j,:]
-                fm.plotting.plot_all([temp], labels, templ_meas=fm.plotting.plot_measurements_insilico, df=data[0].filter(like=med).filter(like=f'_{int(temp):02d}C_'), clrs=exp_clrs,temps=temps_model, mtimes=t_model, mestim=obs_mibi, dir=path2, add_name=f'MiBi_{med}_const_model_{int(temp)}Grad'+add_name, time_lim=[17.5])
-        '''
-
+        # Plot data vs. estimation: x_count
         t_exp, [x_exp] = extract_observables_from_df(data)
         fig, ax = plt.subplots()
         for i in range (n_cl):
@@ -171,6 +146,7 @@ if __name__ == "__main__":
         plt.savefig(path2+f'x_count_{k}exp.png', bbox_inches='tight')
         plt.close(fig)
 
+        # Plot data vs. initial model: x_count
         t_exp, [x_exp] = extract_observables_from_df(data)
         fig, ax = plt.subplots()
         for i in range (n_cl):
@@ -189,6 +165,7 @@ if __name__ == "__main__":
     plt.savefig(path2+'alph_opt_histogram.png', bbox_inches='tight')
     plt.close(fig)
 
+    # Plot Histograms of alpha values:
     fig, ax = plt.subplots()
     ax.hist(np.array(alph_exps).flatten(), bins=20)  # arguments are passed to np.histogram
     plt.savefig(path2+'alph_exps_histogram.png', bbox_inches='tight')
@@ -204,7 +181,7 @@ if __name__ == "__main__":
     mu, sigma = np.log(scale), shape
     print(f"Data: Mu: {mu}, Sigma: {sigma}")
 
-
+    # Plot initial and estimated alpha distriution
     fig, ax = plt.subplots()
     x = np.linspace(0., 5, 100)
     pdf = stats.lognorm.pdf(x, s=sigma_opt, scale=np.exp(mu_opt))
@@ -218,7 +195,7 @@ if __name__ == "__main__":
     plt.savefig(path2+'alph_distribution.png', bbox_inches='tight')
     plt.close(fig)
 
-
+    # Plot comparisom of initial and estimated parameters:
     fig, ax = plt.subplots()
     sct_mrk = ['o', 'x', '^', 's', 'D', 'P', '*', 'h', 'v', '<', '>']
     tick_lbls = [r'$\lambda_1$', r'$\lambda_2$', r'$n_{\max}$', r'k$_{12}$', r'k$_{21}$']
@@ -250,8 +227,8 @@ if __name__ == "__main__":
     plt.savefig(path2+'alpha_comparison_opt_vs_exps.png', bbox_inches='tight')
     plt.close(fig)
 
+    # Plot alpha distribution with sampled values
     plot_sampled_parameter(mu, sigma, [alph_exps[1]], path=path2)
-
 
     ## Plot comparison for different curves of alpha distribution:
     n_exps_vals = [5, 20]
@@ -261,8 +238,7 @@ if __name__ == "__main__":
     pdf = stats.lognorm.pdf(x, s=sigma, scale=np.exp(mu))
     ax.plot(x, pdf, label=f'"Real" distribution\n (mu={mu:.2f}, sigma={sigma:.2f})', color='k', linewidth=3)
     for n in n_exps_vals:
-        path = f'out/main_param_distrib2_{int(n)}exp_divx/'
-        path2=path
+        path2 = f'out/main_param_distrib2_{int(n)}exp_divx/'
         exp_temps = fm.output.read_from_json(''+'exp_temps_model_paper.json', dir=path2)
         res = fm.output.read_from_json('Result_calibration.json', dir=path2)
         param_opt = res['param_ode']

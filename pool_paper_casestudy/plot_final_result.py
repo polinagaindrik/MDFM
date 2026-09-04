810bd99bef2d7e04046f9671926cee928f735a18
polinagaindrik <polinagayndrik@gmail.com>
2026-09-04 15:45:51 +0200

    pool paper: update final plotting files

--- pool_paper_casestudy/plot_final_result.py
+++ pool_paper_casestudy/plot_final_result.py
@@ -20,20 +20,20 @@ def get_param_dfs(path, path2):
 if __name__ == "__main__":
     n_cl = 4
     add_name = ''
-    model = ode_model_coculture3
+    model = ode_model_coculture_wopH_MM
     names = ['Ls23K', 'LsCTC494', 'Lm', 'Ls23K-Lm', 'LsCTC494-Lm']
-    path = 'pool_paper_casestudy/out/all_together_final/'
-    path2 = 'pool_paper_casestudy/out/all_together_final/'
+    path = 'pool_paper_casestudy/out/wo_pH_new/'
+    path2 = 'pool_paper_casestudy/out/wo_pH_new/'
     dfs_saved = pd.read_pickle(path2+'dataframe_poolpaper_all.pkl')
 
     # Monoculture experiments results:
-    param_opt = fm.output.read_from_json('Result_calibration_mono_local.json', dir=path2)["param_ode"]
+    param_opt = fm.output.read_from_json('Result_calibration_mono_MM_local.json', dir=path2)["param_ode"]
     plot_cases_separately(param_opt, dfs_saved, model, path=path2, add_name='_localopt_mono', exp_indexes=[0, 1, 2])
 
     # 3 exps (Lm+Ls23K) results:
-    param_opt = fm.output.read_from_json('Result_calibration_3exps_monoco_local.json', dir=path2)["param_ode"]
+    param_opt = fm.output.read_from_json('Result_calibration_3exps_monoco_MM_local.json', dir=path2)["param_ode"]
     plot_cases_separately(param_opt, dfs_saved, model, path=path2, add_name='_localopt_monoco_3exps', exp_indexes=[0, 2, 3])
 
     # All 5 exps together 
-    param_opt = fm.output.read_from_json('Result_calibration_5exps_local.json', dir=path2)["param_ode"]
+    param_opt = fm.output.read_from_json('Result_calibration_5exps_MM_local.json', dir=path2)["param_ode"]
     plot_cases_separately(param_opt, dfs_saved, model, path=path2, add_name='_localopt_co_5exps')
